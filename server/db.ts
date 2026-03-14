5d824b910dc5e393ae103fcaf4bd626bb2e6cc8d
Guttapalyam <g.muralidhar@dxc.com>
2026-03-14 13:34:42 +0530

    fix: Swap Neon Serverless Driver to use HTTP to prevent Vercel Serverless timeouts

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,6 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from "@shared/schema";
-
-neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
@@ -11,5 +8,8 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle(pool, { schema });
+// Ensure fetch connections aren't cached indefinitely in serverless functions
+neonConfig.fetchConnectionCache = true;
+
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql, { schema });
